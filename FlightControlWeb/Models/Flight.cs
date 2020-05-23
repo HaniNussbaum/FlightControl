ac710d310d715f59098ff87eeb889b655be52239
noa Leibman <noale27@gmail.com>
2020-05-23 16:02:29 +0300

    flights model+controller first

---
+++ FlightControlWeb/Models/Flight.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlightControlWeb.Models
+{
+    public class Flight
+    {
+        public string Id { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public int Passengers { get; set; }
+        public string Company { get; set; }
+        public DateTime DateTime { get; set; }
+        public bool Is_external { get; set; }
+        public List<Dictionary<string, double>> Segments { get; set; }
+    }
+}
