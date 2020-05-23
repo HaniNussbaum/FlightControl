a7b09f953253a2d927b6deeeeb46c2458ee7561e
Hani Nussbaum <hani.nussbaum@live.biu.ac.il>
2020-05-23 16:12:11 +0300

    updated server in models

--- FlightControlWeb/Models/Server.cs
+++ FlightControlWeb/Models/Server.cs
@@ -3,41 +3,15 @@ using Microsoft.Extensions.Caching.Memory;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace FlightControlWeb.Models
 {
-    public class Server
-        
-    {
-        private int serverIndex;
-        private static IMemoryCache _cache;
-        public Server(IMemoryCache cache)
-        {
-            _cache = cache;
-        }
-        public Dictionary<int, JsonResult> getServersInstance()
-        {
-            var servers = new Dictionary<int, JsonResult>();
-            if (!_cache.TryGetValue("ServerList", out servers))
-            {
-                if (servers == null)
-                {
-                    servers = new Dictionary<int, JsonResult>();
-                }
-                _cache.Set("ServerList", servers);
-            }
-            return servers;
-        }
-        public void AddServer(JsonResult server)
-        {
-            var servers = getServersInstance();
-            servers.Add(servers.Count, server);
-        }
-            public void DeleteServer(int ID)
-        {
-            
-        }
+    public class Server { 
+        public int ServerId { get; }
+        public string ServerURL { get; }
+
     }
 
 }
